6310caef191aa768ee6b7995cebe900ff363274b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-12-03 22:57:51 +0100

    async done

--- Lab5/script.js
+++ Lab5/script.js
@@ -71,6 +71,7 @@ function makeNewObject(id, nr) {
                 .then(response => {
                     let companyname = response.works[0].company[0].companyname;
                     let year = response.works[0].year;
+                    console.log("*TAKS 2*");
                     console.log("Nazwa firmy: " + companyname);
                     console.log("Rok pracy: " + year);
                     resolve(newObject(companyname,year));
@@ -87,4 +88,86 @@ btn4.addEventListener("click", function(){
         .then(data => console.log("Nowy obiekt: ", data))
         .catch(error => console.log("Blad", error))
         .finally(() => console.log("Finally"))
+});
+
+// ASYNC 1
+function obliczAsync(id, x, y) {
+    return new Promise((resolve,reject) => {
+        if(id === 1){
+            console.log("*TAKS 1*");
+            console.log("id pracy: " + x);
+            console.log("przepracowane lata: " + y);
+            const sum = x+y;
+            resolve(sum);
+        } else {
+            reject("nie ma takiej pracy");
+        }
+    })
+};
+async function getValuesAsync(id,a,b){
+    try {
+        const response = await fetch("https://my-json-server.typicode.com/jakubzengota/projektowanie-serwisow-www-21684-185ic/users/1");
+        if (response.status === 200 && a >=0 && a <=1 && b >=0 && b<=4) {
+            let jsonObj = await response.json();
+            let workID = jsonObj.works[a].id;
+            let worktime = jsonObj.works[a].company[b].worktime;
+            const data = await obliczAsync(id,workID,worktime);
+            console.log("dodaj: " + data);
+        } else {
+            if(a < 0 || b < 0 || a > 1 || b > 4) {
+                console.log("Brak pracy lub nazwy firmy");
+            } else {
+                console.log("Blad: ", response.status, response.statusText);
+            }
+        }
+    } catch(error) {
+        console.log("Blad");
+    } finally {
+        console.log("Finally");
+    }
+};
+let btn5 = document.getElementById("btn5");
+btn5.addEventListener("click", function(){
+    getValuesAsync(1, 0, 1);
+})
+
+// ASYNC 2
+function makeObjectAsync(id, x, y) {
+    return new Promise((resolve,reject) => {
+        if(id === 1){
+            console.log("*TAKS 2*");
+            const obj = {"companyname": x, "worktime": y};
+            console.log("Nazwa firmy: " + x);
+            console.log("Czas pracy: " + y);
+            resolve(obj);
+        } else {
+            reject("Nie ma usera o takim ID");
+        }
+    })
+};
+async function getObjectsAsync(id, b){
+    try {
+        const response = await fetch("https://my-json-server.typicode.com/jakubzengota/projektowanie-serwisow-www-21684-185ic/users/2");
+        if (response.status === 200 && b >=0 && b<=5) {
+            let jsonObj = await response.json();
+            let companyname = jsonObj.works[0].company[b].companyname;
+            let worktime = jsonObj.works[0].company[b].worktime;
+            const data = await makeObjectAsync(id,companyname, worktime);
+            console.log("Nowy obiekt: ", data);
+        } else {
+            if(b>5 || b<0){
+                console.log("Nie ma takiej firmy");
+            } else {
+                console.log("Blad: ", response.status, response.statusText);
+            }
+        }
+    } catch(error) {
+        console.error(error);
+    } finally {
+        console.log("Finally");
+    }
+};
+let btn6 = document.getElementById("btn6");
+btn6.addEventListener("click", function(){
+    getObjectsAsync(1,0);
 });
